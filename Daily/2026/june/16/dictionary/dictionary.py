36dfb873a6e0a29f875a87a3a8132eedc5f8ee91
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-06-16 21:05:06 -0700

    June 16th Python Daily Practice

---
+++ Daily/2026/june/16/dictionary/dictionary.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+person = {"name": "Cosmos", "age": 30, "city": "Portland"}
+print(person["name"])
---
+++ Daily/2026/june/16/lists/list.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+fruit = ["apple", "banana", "cherry"]
+print(fruit[0])
