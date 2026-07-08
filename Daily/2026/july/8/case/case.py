df4e717753085ee217af24a86e393413f3ce666a
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-07-08 12:51:15 -0700

    July 8th Python3 Daily Practice

---
+++ Daily/2026/july/8/case/case.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def greet():
+    print("1. English") 
+
+    language = input("Entry: ").lower()
+
+    print(language)
+
+greet()
---
+++ Daily/2026/july/8/print/print.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print("hello world!")
+print("my name is cosmos")
+print("2 + 3 =", 2 + 3)
+print("this is line one\nthis is line twwo")
