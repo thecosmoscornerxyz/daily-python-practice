ed42aa30e678832e96a56f794b91dab3c2e7d07b
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-08-15 14:30:51 -0700

    August 15th Python Daily Practice

---
+++ Daily/2026/august/15/case/case.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def greet():
+    print("1. English")
+
+    print("[q] Quit")
+
+    language = input("what is your name? ")
+
+    print(language)
+
+greet()
---
+++ Daily/2026/august/15/dictionary/dictionary.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+person = {"name": "cosmos", "age": 30, "city": "Portland"}
+print(person["name"])
