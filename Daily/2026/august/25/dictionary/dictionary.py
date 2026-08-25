a561285ae4cfc5cd37891c506d7babaacf99ac84
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-08-25 15:16:01 -0700

    August 25th Python Daily Practice

---
+++ Daily/2026/august/25/dictionary/dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+person = {
+    "name": "cosmos",
+    "age": 30,
+    "city": "Portland"
+}
+print(person["name"])
---
+++ Daily/2026/august/25/loops/loop.py
@@ -0,0 +1,4 @@
+#!/usr/bine/nv python3
+
+for i in range(11):
+    print("loop number:", i)
