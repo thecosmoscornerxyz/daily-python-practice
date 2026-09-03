550a57ce6a409b8f9d3cab0eaabda0f97673ec13
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-09-03 14:56:04 -0700

    September 3rd Python Daily Practice

---
+++ Daily/2026/september/3/case/case.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def greet():
+    print("1. English")
+
+    print("[q] Quit")
+
+    language = input("Entry: ")
+
+    if lanuage == "q":
+        return
+
+    print(language)
+
+greet()
---
+++ Daily/2026/september/3/function/function.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("cosmos")
