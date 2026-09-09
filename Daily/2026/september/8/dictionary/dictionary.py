b6074d6661f2043a49ef43fd99ace1e8da70dfd0
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-09-08 17:08:12 -0700

    September 8th Python Daily Practice

---
+++ Daily/2026/september/8/dictionary/dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+person = {
+    "name": "cosmos",
+    "age": 30,
+    "city": "Portland"
+}
+print(person[0])
+person.append("orange")
+print(f"Updated List: {fruit}")
---
+++ Daily/2026/september/8/lists/list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+fruit = [
+    "apple",
+    "banana",
+    "cherry"
+]
+print(fruit[0])
