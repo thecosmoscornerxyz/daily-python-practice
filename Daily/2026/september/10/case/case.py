99a4e597d384e5a4411bc04ec92c1776d7ece5ca
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-09-10 10:24:37 -0700

    September 10th Python Daily Practice

---
+++ Daily/2026/september/10/case/case.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def menu():
+    print("[1] Check Server")
+    print("[2] Restart Service")
+    print(" ")
+    print("[q] Quit")
+
+    choice = input("Entry: ").strip().lower()
+
+    if choice == "1":
+        print("Checking Server...")
+    elif choice == "2":
+        print("Restarting Service...")
+    elif choice == "q":
+        return
+    else:
+        print("Invalid Option")
+
+menu()
