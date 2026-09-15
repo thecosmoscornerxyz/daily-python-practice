723fabac240b185a6a0f9368585330bb387b2c44
Cosmos <92128982+thecosmoscornerxyz@users.noreply.github.com>
2026-09-15 13:55:35 -0700

    September 15th Python Daily Practice

---
+++ Daily/2026/september/15/print/print.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+name = "Cosmos"
+status = "Online"
+users = 42
+
+print(f"Users: {name}")
+print(f"Status: {status} | Users: {users}")
+print("server", "status", "users", sep=" | ")
+print(f"{'Server':<10} {'Status':<10} {'Users':>5}")
